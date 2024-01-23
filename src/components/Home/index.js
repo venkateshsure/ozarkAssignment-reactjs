7f6d7e3a9199148593d109ec42cb61c7c8331e5a
vennkateshsure <venky007sure@gmail.com>
2024-01-23 10:38:52 +0000

    completed Home section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -18,39 +18,129 @@ const Home = () => {
       <Header />
       <div className="slider-container">
         <Slider className="dotsClass" {...settings}>
-          <div className="slider-content-con">
-            <img
-              src="https://img.freepik.com/free-photo/photo-smiling-dentist-standing-with-arms-crossed-with-her-colleague-showing-okay-sign_496169-1043.jpg"
-              alt="teeth-logo"
-              className="carousel-image"
-            />
-            <h1>hii</h1>
+          <div className="back-content-con">
+            <div className="background-image1">
+              <div className="back-content-con2">
+                <p className="back-content-para1-con">
+                  WE ARE <span className="span-con">COVID</span> READY.
+                </p>
+                <p className="back-content-para1-con2">
+                  The corona virus COVID19 pandemic is the defining global
+                  health crisis.
+                </p>
+                <div className="back-button-con">
+                  <button className="back-read-more-btn" type="button">
+                    Read More
+                  </button>
+                  <button className="back-read-more-btn" type="button">
+                    Contact Us
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="slider-content-con">
-            <img
-              src="https://www.shutterstock.com/image-photo/dentist-mirror-woman-check-smile-600nw-2308801975.jpg"
-              alt="teeth-logo"
-              className="carousel-image"
-            />
-            <h1>hii</h1>
+
+          <div className="back-content-con">
+            <div className="background-image2">
+              <div className="back-content-con2">
+                <p className="back-content-para1-con">ONLINE SOLUTION</p>
+                <p className="back-content-para1-con2">
+                  Welcome to <span className="span-con">Dental Office.</span>
+                </p>
+                <div className="back-button-con">
+                  <button className="back-read-more-btn" type="button">
+                    Read More
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="slider-content-con">
-            <img
-              src="https://www.shutterstock.com/image-photo/dentist-holding-educational-model-dental-260nw-2241502389.jpg"
-              alt="teeth-logo"
-              className="carousel-image"
-            />
-            <h1>hii</h1>
+
+          <div className="back-content-con">
+            <div className="background-image3">
+              <div className="back-content-con2">
+                <p className="back-content-para1-con">
+                  THE DENTAL{" "}
+                  <span className="span-con"> READYCARE OF YOUR </span>WHOLE
+                  FAMILY.
+                </p>
+                <p className="back-content-para1-con2">
+                  Your smile is the identity and the Dental Office is committed
+                  to take good care of it.
+                </p>
+                <div className="back-button-con">
+                  <button className="back-read-more-btn" type="button">
+                    Read More
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="slider-content-con">
-            <img
-              className="carousel-image"
-              alt="teeth-logo"
-              src="https://www.shutterstock.com/image-photo/orthodontist-conducts-consultation-on-dental-260nw-2125609112.jpg"
-            />
-            <h1>hii</h1>
+
+          <div className="back-content-con">
+            <div className="background-image4">
+              <div className="back-content-con2">
+                <p className="back-content-para1-con">
+                  WE ARE <span className="span-con">COVID</span> READY.
+                </p>
+                <p className="back-content-para1-con2">
+                  The corona virus COVID19 pandemic is the defining global
+                  health crisis.
+                </p>
+                <div className="back-button-con">
+                  <button className="back-read-more-btn" type="button">
+                    Read More
+                  </button>
+                  <button className="back-read-more-btn" type="button">
+                    Contact Us
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </Slider>
+        <div className="home-head-con">
+          <div className="our-first-con">
+            <p className="para-con1">Our Doctors</p>
+            <p className="para-con2">
+              CARE SOLUTIONS Quality Affordable In-Home Care In Your Community
+              Is Just A Phone Call Away. Learn More About Us Below.
+            </p>
+            <button className="about-button" type="button">
+              About Us
+            </button>
+          </div>
+          <div className="our-second-con">
+            <p className="para-con1">OPENING HOURS</p>
+            <p className="para-con2">
+              Monday04:30 PM - 09:00 PM Tues - Sat09:00 AM - 09:00 PM Sun10:00
+              AM - 02:00 PM
+            </p>
+            <button className="about-button" type="button">
+              Request an appointment
+            </button>
+          </div>
+          <div className="our-first-con">
+            <p className="para-con1">24 HOUR SERVICE</p>
+            <p className="para-con2">
+              DEFINED BY YOU 24 hour phone support for Dental Emergency, clinic
+              Off hours emergency treatment options.
+            </p>
+            <button className="about-button" type="button">
+              Learn More
+            </button>
+          </div>
+          <div className="our-second-con">
+            <p className="para-con1">BOOKING A VISIT</p>
+            <p className="para-con2">
+              AT YOUR CONVENIENCE +91 9971277194 Get The Quality Care That You
+              Deserve Immediately
+            </p>
+            <button className="about-button" type="button">
+              Contact Us
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
