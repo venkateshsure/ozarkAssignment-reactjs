2dc6c2d47ad002824484e4329f9cf456181aa92c
vennkateshsure <venky007sure@gmail.com>
2024-01-23 07:39:46 +0000

    introducing react slick third-party-package

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -20,19 +20,35 @@ const Home = () => {
         <Slider className="dotsClass" {...settings}>
           <div className="slider-content-con">
             <img
-              src="https://thedentaloffice.in/wp-content/uploads/2020/06/logo.png"
+              src="https://img.freepik.com/free-photo/photo-smiling-dentist-standing-with-arms-crossed-with-her-colleague-showing-okay-sign_496169-1043.jpg"
               alt="teeth-logo"
-              className="teeth-image"
+              className="carousel-image"
             />
+            <h1>hii</h1>
           </div>
           <div className="slider-content-con">
-            <h3>2</h3>
+            <img
+              src="https://www.shutterstock.com/image-photo/dentist-mirror-woman-check-smile-600nw-2308801975.jpg"
+              alt="teeth-logo"
+              className="carousel-image"
+            />
+            <h1>hii</h1>
           </div>
           <div className="slider-content-con">
-            <h3>3</h3>
+            <img
+              src="https://www.shutterstock.com/image-photo/dentist-holding-educational-model-dental-260nw-2241502389.jpg"
+              alt="teeth-logo"
+              className="carousel-image"
+            />
+            <h1>hii</h1>
           </div>
           <div className="slider-content-con">
-            <h3>4</h3>
+            <img
+              className="carousel-image"
+              alt="teeth-logo"
+              src="https://www.shutterstock.com/image-photo/orthodontist-conducts-consultation-on-dental-260nw-2125609112.jpg"
+            />
+            <h1>hii</h1>
           </div>
         </Slider>
       </div>
